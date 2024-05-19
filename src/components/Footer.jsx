244bd472a47d9316edd17b932dfe54f7a1520d6a
[Joshua Taiwo] <[joshuataiwo07@gmail.com]>
2024-05-19 21:51:13 +0100

    Updated LinkedIn link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer(){
           {/* <a rel="noreferrer" href="https://medium.com/@joshuataiwo07" target="_blank"><FaMedium /></a> */}
           <a rel="noreferrer" href="https://www.facebook.com/joshuataiwo07" target="_blank"><FaFacebook /></a>
           <a rel="noreferrer" href="https://twitter.com/t__jay07?t=ja8QSHhFMcqDuKay2zQrnA&s=09" target="_blank"><FaX /></a>
-          <a rel="noreferrer" href="https://www.linkedin.com/in/taiwo-joshua-olamilekan-02b42520a/" target="_blank"><FaLinkedin /></a>
+          <a rel="noreferrer" href="https://www.linkedin.com/in/joshua-taiwo-a565442b7/" target="_blank"><FaLinkedin /></a>
           <a rel="noreferrer" href="mailto:joshuataiwo07@gmail.com" target="_blank"><FaEnvelope /></a>
           <a rel="noreferrer" href="https://www.instagram.com/teejay0o7?igshid=YTQwZjQ0NmI0OA==" target="_blank"><FaInstagram /></a>
           <a rel="noreferrer" href="https://api.whatsapp.com/send?phone=+2348103182378" target="_blank"><FaWhatsapp /></a>
