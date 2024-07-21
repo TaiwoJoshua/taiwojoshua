28079b04e95aa65162041a6a429764155be21ca7
[Joshua Taiwo] <[joshuataiwo07@gmail.com]>
2024-07-21 23:48:38 +0100

    Inserted Tawk through useEffect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
-import Certificate from './components/Certificate';
-import Contact from './components/Contact';
-import Services from './components/Services';
-import Footer from './components/Footer';
-import { FaBars, FaChevronDown, FaChevronUp } from 'react-icons/fa6';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+import Certificate from "./components/Certificate";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+import { FaBars, FaChevronDown, FaChevronUp } from "react-icons/fa6";
+import TawkToScript from "./components/TawkToScript";
 
-export default function App(){
-  const timeoutRef = React.useRef(null);  
+export default function App() {
+  const timeoutRef = React.useRef(null);
 
   React.useEffect(() => {
-    function setMove(navoption){
-      const sections = ["#home", "#about", "#portfolio", "#skills", "#certificate", "#services", "#contact"];
+    function setMove(navoption) {
+      const sections = [
+        "#home",
+        "#about",
+        "#portfolio",
+        "#skills",
+        "#certificate",
+        "#services",
+        "#contact",
+      ];
       navoption = navoption.href.slice(navoption.href.indexOf("#"));
       const currSection = sections.indexOf(navoption);
       const prevSection = currSection !== 0 ? currSection - 1 : 0;
-      const nextSection = currSection !== sections.length - 1 ? currSection + 1 : sections.length - 1;
+      const nextSection =
+        currSection !== sections.length - 1
+          ? currSection + 1
+          : sections.length - 1;
       document.getElementById("moveUp").href = sections[prevSection];
       document.getElementById("moveDown").href = sections[nextSection];
     }
-    
+
     function observe(child, sectionId) {
       const section = document.getElementById(sectionId);
-      const navoption = document.querySelector(`#navbar ul li:nth-child(${child}) a`);
+      const navoption = document.querySelector(
+        `#navbar ul li:nth-child(${child}) a`
+      );
       const elementOffset = section.getBoundingClientRect().top;
       const distance = elementOffset;
       const height = parseFloat(getComputedStyle(section).height) - 61;
@@ -38,8 +52,8 @@ export default function App(){
       }
     }
 
-    function allObserve(){
-      if(timeoutRef.current){
+    function allObserve() {
+      if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
       }
 
@@ -59,22 +73,38 @@ export default function App(){
       }, 3000);
     }
 
-    window.addEventListener('scroll', allObserve);
+    window.addEventListener("scroll", allObserve);
     return () => {
-      window.removeEventListener('scroll', allObserve);
+      window.removeEventListener("scroll", allObserve);
 
-      if(timeoutRef.current){
+      if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
       }
-    }
+    };
   }, []);
 
   return (
     <div className="app">
-      <span className='move left'><a href='#home' className="fa-solid fa-chevrons-up"> </a><a href='#footer' className="fa-solid fa-chevrons-down"> </a></span>
-      <span className='move right'><a href='#home' id='moveUp'><FaChevronUp /></a><a href='#about' id='moveDown'><FaChevronDown /></a></span>
-      <input type="checkbox" id="showNav" style={{display: "none"}} />
-      <label htmlFor="showNav" id="menu"><FaBars /></label>
+      <span className="move left">
+        <a href="#home" className="fa-solid fa-chevrons-up">
+          {" "}
+        </a>
+        <a href="#footer" className="fa-solid fa-chevrons-down">
+          {" "}
+        </a>
+      </span>
+      <span className="move right">
+        <a href="#home" id="moveUp">
+          <FaChevronUp />
+        </a>
+        <a href="#about" id="moveDown">
+          <FaChevronDown />
+        </a>
+      </span>
+      <input type="checkbox" id="showNav" style={{ display: "none" }} />
+      <label htmlFor="showNav" id="menu">
+        <FaBars />
+      </label>
       <label className="nav-close" htmlFor="showNav"></label>
       <Navbar />
       <Home />
@@ -85,6 +115,7 @@ export default function App(){
       <Services />
       <Contact />
       <Footer />
+      <TawkToScript />
     </div>
   );
 }
---
+++ src/components/TawkToScript.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+const TawkToScript = () => {
+  useEffect(() => {
+    window.Tawk_API = window.Tawk_API || {};
+    window.Tawk_LoadStart = new Date();
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.tawk.to/666ecf5c981b6c56477de2ec/1i0gdk0pf";
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+};
+
+export default TawkToScript;
